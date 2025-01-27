88e87e4dc984506f723945c0cd993f0aab2436c7
abhishek <abhishekpateria77@gmail.com>
2025-01-27 08:50:16 +0530

    Deleted HackerRank_SQL

--- HackerRank_SQL/City1.sql
+++
@@ -1,6 +0,0 @@
-Select * from city
-select (max(population) - min(population)) from city
-Select SUM(Population) from city where COuntryCOde ='JPN';
-Select Avg(Population) from city where District ='California';
---select Count(Name) from City where population>150000;
-select sum(Population) from city where District ='California';
